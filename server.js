42504a05ed22079749ff22ff3b71fde4d23fc1ef
Tlangelani Mboweni <tlangelani_mboweni@yahoo.com>
2016-04-09 00:51:48 +0200

    add view dir

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ var express     = require('express'),
     app         = express(),
     port        = 3000;
 
+app.set('env', 'development');
+
+app.set('view engine', 'jade');
+app.set('views', 'views');
+
 // parse all body content to routes
 app.use(bodyParser.urlencoded());
 
+var names = [];
+
 app.get('/', function(req, res) {
-    res.render('index.jade', { names: names });
+    res.render('index.jade', {
+        names: names
+    });
 });
 
 app.post('/', function(req, res) {
